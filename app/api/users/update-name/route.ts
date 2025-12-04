1c2a6debc7f5866ea84059208a04aae1a6fa19e4
User <user@example.com>
2025-12-04 23:53:49 +1100

    100

--- app/api/users/update-name/route.ts
+++ app/api/users/update-name/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth/auth';
 import type { Session } from 'next-auth';
 
 // PATCH /api/users/update-name - Update the current user's name
